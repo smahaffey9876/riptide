d99fe67084efa85579fa0761aa4fee4a0b641abb
Conor Cary <cary.42@osu.edu>
2016-06-10 17:09:37 -0400

    The new PIDs work now, woohoo!

--- riptide_navigation/src/attitude_controller.cpp
+++ riptide_navigation/src/attitude_controller.cpp
@@ -11,12 +11,13 @@
 #include "tf/LinearMath/Transform.h"
 #include <tf/transform_listener.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/AccelStamped.h>
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/Vector3.h>
 
-double px,ix,dx,imx,dmx,py,iy,dy,imy,dmy,pz,iz,dz,imz,dmz,pr,ir,dr,imr,dmr,pp,ip,dp,imp,dmp,pyaw,iyaw,dyaw,imyaw,dmyaw;
+double px,ix,dx,imx,dmx,py,iy,dy,imy,dmy,pz,iz,dz,imz,dmz;
 bool first_reconfig= true;
-ros::Publisher p_e;
+ros::Publisher aa_e;
 tf::StampedTransform transform;
 
 void dyn_callback(riptide_navigation::pidConfig &config, uint32_t level) {
@@ -46,44 +47,38 @@ dmz=config.dmz;
 void callback(const nav_msgs::Odometry::ConstPtr& current_pose, const geometry_msgs::PoseStamped::ConstPtr& pose_set)
 {
 
-  geometry_msgs::Vector3 position_des, position_current, orientation_des, orientation_current;
-  geometry_msgs::PoseStamped pose_error;
+  geometry_msgs::AccelStamped ang_accel_error;
   ros::Time time;
   ros::Duration time_diff;
   ros::Time last_time;
-    
+  
   control_toolbox::Pid pidx, pidy, pidz;
 
   pidx.initPid(px,ix,dx,imx,dmx);
   pidy.initPid(py,iy,dy,imy,dmy);
   pidz.initPid(pz,iz,dz,imz,dmz);
-
-
+  
   time = ros::Time::now();
   time_diff = time-last_time;
-  pose_error.header.stamp = time;
-  
-  pose_error.pose.position.x=pidx.control_toolbox::Pid::computeCommand((pose_set->pose.position.x-current_pose->pose.pose.position.x),time_diff);
-  pose_error.pose.position.y=pidy.control_toolbox::Pid::computeCommand((pose_set->pose.position.y-current_pose->pose.pose.position.y),time_diff);
-  pose_error.pose.position.z=pidz.control_toolbox::Pid::computeCommand((pose_set->pose.position.z-current_pose->pose.pose.position.z),time_diff);
-  
-  pose_error.pose.orientation.x = pose_set->pose.orientation.x;
-  pose_error.pose.orientation.y = pose_set->pose.orientation.y;
-  pose_error.pose.orientation.z = pose_set->pose.orientation.z;
+  ang_accel_error.header.stamp = time;
 
+  ang_accel_error.accel.angular.x=pidx.control_toolbox::Pid::computeCommand((pose_set->pose.orientation.x-current_pose->pose.pose.orientation.x),time_diff);
+  ang_accel_error.accel.angular.y=pidy.control_toolbox::Pid::computeCommand((pose_set->pose.orientation.y-current_pose->pose.pose.orientation.y),time_diff);
+  ang_accel_error.accel.angular.z=pidz.control_toolbox::Pid::computeCommand((pose_set->pose.orientation.z-current_pose->pose.pose.orientation.z),time_diff);
+  
   last_time = time;
 
-  p_e.publish(pose_error);
+  aa_e.publish(ang_accel_error);
 }
 
 int main(int argc, char **argv) {
 
-  ros::init(argc, argv, "position_controller");
+  ros::init(argc, argv, "attitude_controller");
   ros::NodeHandle nh;
   ros::NodeHandle node_priv("~");
 
   message_filters::Subscriber<nav_msgs::Odometry> kalmanout_sub(nh, "/odometry/filtered",1);
-  message_filters::Subscriber<geometry_msgs::PoseStamped> poseset_sub(nh, "pose_set_pt",1);
+  message_filters::Subscriber<geometry_msgs::PoseStamped> poseset_sub(nh, "pose_error",1);
  
   dynamic_reconfigure::Server<riptide_navigation::pidConfig> server;
   dynamic_reconfigure::Server<riptide_navigation::pidConfig>::CallbackType f;
@@ -94,7 +89,7 @@ int main(int argc, char **argv) {
   message_filters::Synchronizer<MySyncPolicy> sync(MySyncPolicy(100), kalmanout_sub, poseset_sub);  
   sync.registerCallback(boost::bind(&callback,_1,_2));
 
-  p_e = nh.advertise<geometry_msgs::PoseStamped>("pose_error", 1);
+  aa_e = nh.advertise<geometry_msgs::AccelStamped>("ang_accel_error", 1);
   tf::TransformListener listener;  
  
   ros::Rate rate(20.0);
